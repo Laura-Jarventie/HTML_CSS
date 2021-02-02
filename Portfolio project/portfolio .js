229f0a16f696a0a67f6d00b1d35a689c118c714e
Laura Järventie <s2100135@edu.bc.fi>
2021-02-02 15:20:35 +0200

    tuesday

---
+++ Portfolio project/portfolio .js
@@ -0,0 +1,10 @@
+const setBackground = (event) => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementsByName("header").portfolio.background-image = "url(https://source.unsplash.com/IBEXUZBmlXg/200x200)";
+      } else {
+        document.getElementsByClassName("div.fixed").background-color = "#30d6cc";
+      }
+}
+
+
+form.addEventListener("scroll", setBackground);
