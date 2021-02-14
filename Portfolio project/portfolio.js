0f37d38fb0a1cf11763cbbc8a7bd9584e2f48bb1
Laura Järventie <s2100135@edu.bc.fi>
2021-02-14 18:05:46 +0200

    sun

--- Portfolio project/portfolio.js
+++ Portfolio project/portfolio.js
@@ -16,3 +16,17 @@ const scrollNav = () => {
 window.addEventListener("scroll", scrollNav);
 
 // Point is that you have bg color transp. or colored.
+
+mybutton = document.getElementById("myBtn");
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  
+  document.documentElement.scrollTop = 0;
